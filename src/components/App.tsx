e331649b363dcab65f1f3cce77f83879e75dce19
Elif Varli <elifyilbasi@gmail.com>
2026-01-30 09:45:39 +0100

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import Container from "./Container";
+import HashtagList from "./HashtagList";
+
+function App() {
+  return (
+    <div className="app">
+      <Container />
+
+      <HashtagList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HashtagList.tsx
@@ -0,0 +1,15 @@
+export default function HashtagList() {
+  return (
+    <ul className="hashtags">
+      <li>
+        <button>#Starbucks</button>
+      </li>
+      <li>
+        <button>#Nike</button>
+      </li>
+      <li>
+        <button>#Adidas</button>
+      </li>
+    </ul>
+  );
+}
